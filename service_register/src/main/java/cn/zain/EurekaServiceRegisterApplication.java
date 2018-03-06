e0dfd79fd0ab1da1b701406df7d25755fb4d40c3
yongzhian <yongzhian@163.com>
2018-03-06 16:34:34 +0800

    注册中心高可用

--- service_register/src/main/java/cn/zain/EurekaServiceRegisterApplication.java
+++ service_register/src/main/java/cn/zain/EurekaServiceRegisterApplication.java
@@ -14,4 +14,5 @@ public class EurekaServiceRegisterApplication {
     public static void main(String[] args) {
         SpringApplication.run(EurekaServiceRegisterApplication.class, args);
     }
+
 }
